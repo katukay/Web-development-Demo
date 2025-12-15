41466a02f0862dcc57022d9e03330ab9b532ac4e
kanu <katukay@yahoo.com>
2025-12-15 16:07:50 +0000

    fix the message problem

---
+++ message.sh
@@ -0,0 +1,6 @@
+#! /bin/bash 
+echo "we had a heavy snow yesterday"
+echo "my kids have a two hour school delay today"
+echo "my car and my wifes car was covered in a snow and ice"
+echo "most employees at my job came to work late due to heavy snow fall."x
+
